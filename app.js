6928761efc36034922e74feea57e895bd2d9a966
Cezar Augusto Molinar <cezarmolinar@gmail.com>
2026-02-22 18:21:32 -0300

    CRUD Clientes

--- app.js
+++ app.js
@@ -14,11 +14,13 @@ app.use(express.json());
 
 // Define a porta do servidor
 const PORT = 3000;
+let dadosClientes = [];
+let proximoId = 1;
 
 // Rota inicial (rota de teste)
 app.get('/', (req, res) => {
   res.json({
-    message: 'API Node.js com Express está funcionando!'
+    message: 'API Node.js com Express está funcionando!',
   });
 });
 
@@ -27,8 +29,92 @@ app.get('/health', (req, res) => {
   res.json({
     status: 'ok',
     uptime: process.uptime(),
-    timestamp: new Date()
+    timestamp: new Date(),
   });
+});
+
+app.get('/clientes', (req, res) => {
+  res.status(200).json(dadosClientes);
+});
+
+app.get('/clientes/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const cliente = dadosClientes.find((c) => c.id === id);
+
+  if (!cliente) {
+    return res.status(404).json({ mensagem: 'Cliente não encontrado' });
+  }
+
+  res.json(cliente);
+});
+
+app.post('/clientes', (req, res) => {
+  const cliente = req.body;
+  dadosClientes.push(cliente);
+  res.status(201).json({ message: 'Cliente criado com sucesso', cliente });
+});
+
+app.post('/clientes', (req, res) => {
+  const { nome, email } = req.body;
+
+  const novoCliente = {
+    id: proximoId++,
+    nome,
+    email,
+  };
+
+  dadosClientes.push(novoCliente);
+
+  res.status(201).json(novoCliente);
+});
+
+app.put('/clientes/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const { nome, email } = req.body;
+
+  const index = dadosClientes.findIndex((c) => c.id === id);
+
+  if (index === -1) {
+    return res.status(404).json({ mensagem: 'Cliente não encontrado' });
+  }
+
+  dadosClientes[index] = {
+    id,
+    nome,
+    email,
+  };
+
+  res.json(dadosClientes[index]);
+});
+
+app.patch('/clientes/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const cliente = dadosClientes.find((c) => c.id === id);
+
+  if (!cliente) {
+    return res.status(404).json({ mensagem: 'Cliente não encontrado' });
+  }
+
+  const { email } = req.body;
+
+  cliente.email = email;
+
+  res.json(cliente);
+});
+
+app.delete('/clientes/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const index = dadosClientes.findIndex((c) => c.id === id);
+
+  if (index === -1) {
+    return res.status(404).json({ mensagem: 'Cliente não encontrado' });
+  }
+
+  dadosClientes.splice(index, 1);
+
+  res.json({ mensagem: 'Cliente removido com sucesso' });
 });
 
 
